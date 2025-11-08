1ef13ca01b99ff23da12b2512b019439c3b6e715
FedericoCWW <federicocandow@hotmail.com>
2025-11-08 19:20:17 -0300

    8_11_2025_2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import Map from './componentes/Map'
+import { useState, useEffect } from "react";
+import Map from "./componentes/Map";
 
 function App() {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+      const res = await fetch(
+        "https://eonet.gsfc.nasa.gov/api/v3/events?limit=20"
+      );
+      const data = await res.json();
+      setEventData(data);
+      setLoading(false);
+    };
+
+    fetchEvents();
+  }, []);
+
+  useEffect(() => {
+    console.log("Event data updated:", eventData);
+  }, [eventData]);
   return (
     <div className="App">
-        <Map/>
+      { !loading ? <Map eventData={eventData}/> : <h1>Cargando...</h1>}
+      
     </div>
   );
 }
